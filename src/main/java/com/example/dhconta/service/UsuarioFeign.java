830bdbbb19024d975f3231d18474eab463012b4b
Tulio Viglione <tulio.l.viglione@gmail.com>
2022-06-03 18:23:36 -0300

    Alterado estrategia load balance
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   src/main/java/com/example/dhconta/conf/LoadBalancerConfiguration.java
    	modified:   src/main/java/com/example/dhconta/service/UsuarioFeign.java

--- src/main/java/com/example/dhconta/service/UsuarioFeign.java
+++ src/main/java/com/example/dhconta/service/UsuarioFeign.java
@@ -1,10 +1,13 @@
 package com.example.dhconta.service;
 
+import com.example.dhconta.conf.LoadBalancerConfiguration;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name= "dh-usuario")
+@LoadBalancerClient(name = "dh-usuario",configuration= LoadBalancerConfiguration.class)
 public interface UsuarioFeign {
     @RequestMapping(method = RequestMethod.GET,value = "/usuario")
     String findUsuario();
